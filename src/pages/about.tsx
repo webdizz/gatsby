9f317678d5fee17d0147ed5455fee8b1fe209f1f
Izzet Mustafaiev <izzet@mustafaiev.com>
2023-01-02 15:26:59 +0000

    GraphQL

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,13 +2,40 @@ import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import Layout from "../components/Layout";
 import CarImage from "../components/CarImage";
+import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-const AboutPage: React.FC<PageProps> = () => {
+const AboutPage: React.FC<PageProps> = ({ data }) => {
   return (
     <Layout>
       <CarImage />
+      {data.allFile.edges.map(({ node }) => (
+        <GatsbyImage image={getImage(node)} alt={node.name} key={node.id} />
+      ))}
     </Layout>
   );
 };
+
+export const query = graphql`
+  query HomePageQuery {
+    allFile(
+      filter: {
+        sourceInstanceName: { eq: "images" }
+        relativeDirectory: { eq: "cars" }
+      }
+    ) {
+      edges {
+        node {
+          relativePath
+          childImageSharp {
+            gatsbyImageData
+          }
+          id
+          name
+        }
+      }
+    }
+  }
+`;
 
 export default AboutPage;
